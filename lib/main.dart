d2bb32abad06c6b316b9e7dc6caa509347c98dce
Max Vigueras Tovar <maxviguerastovar@Maxs-MacBook-Pro.local>
2023-07-12 23:19:31 -0600

    first commit -- project creation/color settings

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/login/login_page.dart';
+import 'package:flutter_delivery/src/utils/my_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key}); 
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery App Flutter ',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => const LoginPage()
+      },
+      theme: ThemeData(
+        colorScheme:ColorScheme.light(primary: MyColors.primaryColor)
+      ),
+    );
+  }
+}
